15c8b0e63ffa144e0f16c2bf2b3ddded1713f704
Taylor Brodarick <tbrodarick@gmail.com>
2020-08-04 16:02:41 -0500

    Add files via upload

---
+++ Contract_Code/DeferredEquityPlan.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+// Level 3: Deferred Equity 
+contract DeferredEquityPlan {
+    address human_resources;
+
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    // @TODO: Set the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribution = 250;
+    uint start_time = now; // permanently stores time of contract initialization
+    uint public unlock_time = now + 365 days;
+    uint public distributed_shares; // starts at 0
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+        require(unlock_time <= now, "Shares have not vested yet");
+        require(distributed_shares < total_shares, "Share have been completely distributed");
+        
+        unlock_time += 365 days;
+
+        distributed_shares = (now - start_time) / 365 days * annual_distribution;
+
+        // double check in case the employee does not cash out until after 5+ years
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
+
+    // HR and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
+
+    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
+}
